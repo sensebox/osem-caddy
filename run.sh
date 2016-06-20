bb4f83424fec511be20cef9a1d279280f0fd91d6
ubergesundheit <ubergesundheit@googlemail.com>
2016-06-20 18:59:41 +0200

    use too many  to achieve correct substitution

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ if [[ -n "$use_staging_ca" ]]; then
   use_staging_ca="-ca \"https://acme-staging.api.letsencrypt.org/directory\""
 fi
 
-sed -i -e "s|API_DOMAIN|$api_domain|" -e "s|WEB_DOMAIN|$osem_domain|" -e "s|ISSUER_ADDRESS|$issuer_adr|" /etc/caddy/Caddyfile
+sed -i -e "s|API_DOMAIN|$api_domain|" -e "s|\.WEB_DOMAIN:|.$osem_domain:|" -e "s|\.WEB_DOMAIN|.$osem_domain|" -e "s|WEB_DOMAIN|$osem_domain|" -e "s|ISSUER_ADDRESS|$issuer_adr|" /etc/caddy/Caddyfile
 
 execstr="/usr/bin/caddy $use_staging_ca -conf /etc/caddy/Caddyfile"
 
