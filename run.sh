6d088b9f095c49fd1fd58b685c19be3d4964ad33
ubergesundheit <ubergesundheit@googlemail.com>
2017-03-05 19:42:36 +0100

    use caddy built in env expansion

--- run.sh
+++ run.sh
@@ -3,9 +3,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-api_domain=${API_DOMAIN:-api.localhost.localdomain}
-osem_domain=${WEB_DOMAIN:-localhost.localdomain}
-issuer_adr=${ISSUER_ADDRESS:-}
 use_staging_ca=${USE_STAGING_CA:-}
 
 if [[ "$use_staging_ca" == "true" ]]; then
@@ -14,8 +11,6 @@ else
   use_staging_ca=""
 fi
 
-sed -i -e "s|API_DOMAIN|$api_domain|" -e "s|\.WEB_DOMAIN:|.$osem_domain:|" -e "s|\.WEB_DOMAIN|.$osem_domain|" -e "s|WEB_DOMAIN|$osem_domain|" -e "s|ISSUER_ADDRESS|$issuer_adr|" /etc/caddy/Caddyfile
-
-execstr="/usr/bin/caddy $use_staging_ca -conf /etc/caddy/Caddyfile -quic"
+execstr="/usr/bin/caddy $use_staging_ca -agree -conf /etc/caddy/Caddyfile -quic"
 
 eval exec "$execstr"
